c0884ccf28d557b374937c89edc5c0017039d6bf
romajc84 <joeyroman84@gmail.com>
2022-10-16 08:23:51 -0400

    add bloc

---
+++ lib/bloc/timer_event.dart
@@ -0,0 +1,38 @@
+part of 'timer_bloc.dart';
+
+abstract class TimerEvent extends Equatable {
+  const TimerEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class TimerStarted extends TimerEvent {
+  const TimerStarted({required this.duration});
+  final int duration;
+}
+
+class TimerPaused extends TimerEvent {
+  const TimerPaused();
+}
+
+class TimerResumed extends TimerEvent {
+  const TimerResumed();
+}
+
+class TimerReset extends TimerEvent {
+  const TimerReset();
+}
+
+class TimerTicked extends TimerEvent {
+  const TimerTicked({required this.duration});
+  final int duration;
+
+  @override
+  List<Object> get props => [duration];
+}
+
+class TimerChanged extends TimerEvent {
+  const TimerChanged({required this.duration});
+  final int duration;
+}
